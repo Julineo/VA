74edcd69714d59f14aefd2042c347d86c59d7a0f
Julineo <advoretskiy80@gmail.com>
2018-08-03 15:49:04 -0700

    added file parsing

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"bufio"
+//	"strings"
 )
 
 func main() {
@@ -21,15 +22,40 @@ func main() {
 
 func readFile(s string) {
 	fmt.Println("./input/" + s)
+
 	file, err := os.Open("./input/" + s)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	flag := false
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		s := scanner.Text()
+
+		//reading particular values from file
+		if len(s) == 0 {
+			flag = false
+		}
+		if flag {
+			code := s[2:5]
+			name := s[37:70]
+			deduc := s[70:80]
+			remarks := s[84:111]
+			fmt.Println(code)
+			fmt.Println(name)
+			fmt.Println(deduc)
+			fmt.Println(remarks)
+		}
+
+		if len(s) > 2 {
+			if s[2:6] == "CODE" {
+				flag = true
+				continue
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
