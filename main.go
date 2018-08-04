b2a221293374c4c846fe3c08007543a07cac1522
Julineo <advoretskiy80@gmail.com>
2018-08-03 17:17:14 -0700

    added XLSX library

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"os"
 	"bufio"
-//	"strings"
+	"strings"
+
+	"github.com/tealeg/xlsx"
 )
 
 func main() {
@@ -18,6 +20,22 @@ func main() {
 	for _, f := range files {
 		readFile(f.Name())
 	}
+	
+	readDict()
+}
+
+func readDict() error {
+	var mySlice [][][]string
+	var value string
+	mySlice, err := xlsx.FileToSlice("dict.xlsx")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	value = mySlice[0][0][0]
+	_ = value
+	fmt.Println(value)
+	return nil
 }
 
 func readFile(s string) {
@@ -35,26 +53,31 @@ func readFile(s string) {
 	for scanner.Scan() {
 		s := scanner.Text()
 
-		//reading particular values from file
-		if len(s) == 0 {
-			flag = false
-		}
-		if flag {
-			code := s[2:5]
-			name := s[37:70]
-			deduc := s[70:80]
-			remarks := s[84:111]
-			fmt.Println(code)
-			fmt.Println(name)
-			fmt.Println(deduc)
-			fmt.Println(remarks)
-		}
-
-		if len(s) > 2 {
+		if len(s) > 5 {
 			if s[2:6] == "CODE" {
 				flag = true
 				continue
 			}
+		}
+
+		if len(s) == 0 {
+			flag = false
+			continue
+		}
+		if len(s) < 82 {
+			flag = false
+		}
+
+		//reading particular values from file
+		if flag {
+			code := strings.Trim(s[2:5], " ")
+			name := strings.Trim(s[37:70], " ")
+			deduc := strings.Trim(s[70:80], " ")
+			remarks := strings.Trim(s[84:111], " ")
+			fmt.Println(code)
+			fmt.Println(name)
+			fmt.Println(deduc)
+			fmt.Println(remarks)
 		}
 	}
 
